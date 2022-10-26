59e495de7583a31599bd49df9a9f957ae7d80516
= <=>
2022-10-26 11:18:25 -0300

    Chapter 3: Adding a custom and empty Odoo module (Real Estate) to start to play with.

---
+++ odoo-15.0/custom_addons/estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'version': '15.0.0.1.0.0',
+    'author': 'Giovanni Dilly',
+    'category': 'Uncategorized',
+    'summary': 'Real Estate module',
+    'description': """
+        A beginner's module from Odoo tutorial.
+    """,
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    "application": True,
+    'license': 'LGPL-3',
+}
