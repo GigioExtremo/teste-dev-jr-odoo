38419c4b8599abe1dd1bb98f90195ce2f2e270c8
= <=>
2022-10-26 19:20:36 -0300

    Chapter 7: Adding tree, form and search views to the estate_properties for better data visualization.

--- odoo-15.0/custom_addons/estate/models/estate_property.py
+++ odoo-15.0/custom_addons/estate/models/estate_property.py
@@ -6,26 +6,26 @@ class TestModel(models.Model):
     _name = "estate.property"
     _description = "Real Estate Model"
 
-    active = fields.Boolean(string="active", default=True)
-    name = fields.Char(string="name", default="Unknown", required=True)
+    active = fields.Boolean(string="Active", default=True)
+    name = fields.Char(string="Title", default="Unknown", required=True)
     last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
-    description = fields.Text(string="description")
-    postcode = fields.Char(string="postcode")
-    date_availability = fields.Date(string="date_availability",
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available From",
                                     # default value: today's date plus 3 months
                                     default=lambda self: fields.Date.add(fields.Date.today(), relativedelta(+3)))
-    expected_price = fields.Float(string="expected_price", required=True)
-    selling_price = fields.Float(string="selling_price")
-    state = fields.Selection(string="state",
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True)
+    state = fields.Selection(string="State",
                              selection=[("new", "New"), ("offer_received", "Offer Received"),
                                         ("offer_accepted", "Offer Accepted"),
                                         ("sold", "Sold"), ("canceled", "Canceled")], default="new")
-    bedrooms = fields.Integer(string="bedrooms", default=2)
-    living_area = fields.Integer(string="living_area")
-    facades = fields.Integer(string="facades")
-    garage = fields.Boolean(string="garage")
-    garden = fields.Boolean(string="garden")
-    garden_area = fields.Integer(string="garden_area")
-    garden_orientation = fields.Selection(string="garden_orientation",
+    bedrooms = fields.Integer(string="Bedrooms", default=2)
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(string="Garden Orientation",
                                           selection=[("north", "North"), ("south", "South"), ("east", "East"),
                                                      ("west", "West")])
